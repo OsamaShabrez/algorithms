3e2acde5cde756bfd85ce2842503f47571154831
Muhmmad Osama Shabrez <osamashabrez@live.com>
2022-11-02 21:31:26 +0100

    feat(UnitTests): Update unit tests to use Theory and MemberData

--- UnitTests/UnitTestSorting.cs
+++ UnitTests/UnitTestSorting.cs
@@ -6,77 +6,81 @@ namespace UnitTests;
 
 public class UnitTestSorting
 {
-    private readonly int[] _t1 = { 800, 11, 50, 771, 649, 770, 240, 9, 15, 7, 999, 72, 37 };
-    private readonly int[] _r1 = { 7, 9, 11, 15, 37, 50, 72, 240, 649, 770, 771, 800, 999 };
+    public static IEnumerable<object> Data =>
+        new List<object>
+        {
+            new object[]
+            {
+                new[] { 800, 11, 50, 771, 649, 770, 240, 9, 15, 7, 999, 72, 37 },
+                new[] { 7, 9, 11, 15, 37, 50, 72, 240, 649, 770, 771, 800, 999 }
+            },
+            new object[]
+            {
+                new[] { 5, 7, -9, 3, -4, 2, 8 },
+                new[] { -9, -4, 2, 3, 5, 7, 8 }
+            },
+            new object[]
+            {
+                new[] { 10, 8, 7, 3, 5, 1, 9, 2, 4, 6 },
+                new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }
+            }
+        };
 
-    private readonly int[] _t2 = { 5, 7, -9, 3, -4, 2, 8 };
-    private readonly int[] _r2 = { -9, -4, 2, 3, 5, 7, 8 };
-
-    private readonly int[] _t3 = { 10, 8, 7, 3, 5, 1, 9, 2, 4, 6 };
-    private readonly int[] _r3 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-    [Fact]
-    public void TestBubbleSort()
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void TestBubbleSort(int[] input, int[] output)
     {
-        var result = BubbleSort.Iterative(_t1);
-        CollectionAssert.AreEqual(_r1, result);
+        var result = BubbleSort.Iterative(input);
+        CollectionAssert.AreEqual(output, result);
 
-        result = BubbleSort.Recursive(_t2);
-        CollectionAssert.AreEqual(_r2, result);
-
-        result = BubbleSort.Recursive(_t3);
-        CollectionAssert.AreEqual(_r3, result);
-    }
-    
-    [Fact]
-    public void TestMergeSort()
-    {
-        var result = MergeSort.Iterative(_t1);
-        CollectionAssert.AreEqual(_r1, result);
-
-        result = MergeSort.Recursive(_t2);
-        CollectionAssert.AreEqual(_r2, result);
-
-        result = MergeSort.Recursive(_t3);
-        CollectionAssert.AreEqual(_r3, result);
+        result = BubbleSort.Recursive(input);
+        CollectionAssert.AreEqual(output, result);
     }
 
-    [Fact]
-    public void TestRadixSort()
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void TestMergeSort(int[] input, int[] output)
     {
-        var result = RadixSort.Iterative(_t1);
-        CollectionAssert.AreEqual(_r1, result);
+        var result = MergeSort.Iterative(input);
+        CollectionAssert.AreEqual(output, result);
 
-        result = RadixSort.Recursive(_t2);
-        CollectionAssert.AreEqual(_r2, result);
-
-        result = RadixSort.Recursive(_t3);
-        CollectionAssert.AreEqual(_r3, result);
+        result = MergeSort.Recursive(input);
+        CollectionAssert.AreEqual(output, result);
     }
 
-    [Fact]
-    public void TestQuickSort()
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void TestRadixSort(int[] input, int[] output)
     {
-        var result = QuickSort.Iterative(_t1);
-        CollectionAssert.AreEqual(_r1, result);
+        // todo update RadixSort to handle negative integers
+        if (input.Min() < 0) return;
 
-        result = QuickSort.Recursive(_t2);
-        CollectionAssert.AreEqual(_r2, result);
+        var result = RadixSort.Iterative(input);
+        CollectionAssert.AreEqual(output, result);
 
-        result = QuickSort.Recursive(_t3);
-        CollectionAssert.AreEqual(_r3, result);
+        result = RadixSort.Recursive(input);
+        CollectionAssert.AreEqual(output, result);
     }
 
-    [Fact]
-    public void TestSelectionSort()
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void TestQuickSort(int[] input, int[] output)
     {
-        var result = SelectionSort.Iterative(_t1);
-        CollectionAssert.AreEqual(_r1, result);
+        var result = QuickSort.Iterative(input);
+        CollectionAssert.AreEqual(output, result);
 
-        result = SelectionSort.Recursive(_t2);
-        CollectionAssert.AreEqual(_r2, result);
+        result = QuickSort.Recursive(input);
+        CollectionAssert.AreEqual(output, result);
+    }
 
-        result = SelectionSort.Recursive(_t3);
-        CollectionAssert.AreEqual(_r3, result);
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void TestSelectionSort(int[] input, int[] output)
+    {
+        var result = SelectionSort.Iterative(input);
+        CollectionAssert.AreEqual(output, result);
+
+        result = SelectionSort.Recursive(input);
+        CollectionAssert.AreEqual(output, result);
     }
 }
